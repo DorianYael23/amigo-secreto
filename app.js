a21fd1926a50fca3bc4e4c66e0e67ba427229556
DorianYael23 <zdorianyael@gmail.com>
2025-02-05 21:43:19 -0600

    Agrego validaciones y cambio nombre del array

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 // Agregar normbres a una lista y mostrarlos en pantalla despues de agregarlos
 
-let nombreDeAmigos = [];
+let amigos = [];
 function agregarAmigo() {
-    let nombre = document.getElementById('amigo').value;
-    nombreDeAmigos.push(nombre);
+  let nombre = document.getElementById("amigo").value;
+  if (nombre === "") {
+    alert("Por favor, inserte un nombre.");
+  } else {
+    amigos.push(nombre);
     console.log(nombre);
     limpiarCaja();
+  }
+}
+
+function recorrerLista() {
+    
 }
 
 function limpiarCaja() {
-    document.getElementById('amigo').value = '';
+  document.getElementById("amigo").value = "";
 }
